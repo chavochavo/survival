e652d4226ffc9f32ad55628ea03664e19347059f
Yuossef Esper Mendoza <yuossef96@gmail.com>
2016-12-18 18:33:08 -0600

    Se creo el bootstrap del proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name survivalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the survivalApp
+ */
+angular.module('survivalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name survivalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the survivalApp
+ */
+angular.module('survivalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
