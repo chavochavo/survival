8485f3b98e7d3c05526d758295cec617a27037cb
Yuossef Esper Mendoza <yuossef96@gmail.com>
2016-12-30 14:27:13 -0600

    Se corrigieron errores en integracion con node modules y bower components

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-translate/angular-translate.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
